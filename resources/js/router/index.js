c9d8d8b3716c36d8ef411b0780916204f82f73f2
Bencsik András <acelbeton0101@gmail.com>
2025-10-13 14:23:25 +0200

    feat: Created a register view and made a route for it

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,9 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
+import RegisterView from "../views/RegisterView.vue";
 
 const routes = [
     { path: '/', component: HomeView },
+    { path: '/register', component: RegisterView },
 ]
 
 export const router = createRouter({
---
+++ resources/js/views/RegisterView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import TextInput from "../components/TextInput.vue";
+</script>
+
+<template>
+    <div class="p-10">
+        <TextInput model-value=""></TextInput>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
