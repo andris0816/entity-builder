d9329019996d5e802ae269b177cf321f99e67bec
Bencsik András <acelbeton0101@gmail.com>
2025-10-13 14:23:47 +0200

    feat: started implementing an input component

---
+++ resources/js/components/TextInput.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+    interface Props {
+        paceHolder?: string;
+        modelValue: string;
+        customClass?: string;
+    }
+
+    const props = defineProps<Props>();
+    const emit = defineEmits<{
+        'update:modelValue': [value: string]
+    }>();
+</script>
+
+<template>
+    <input
+        :value="modelValue"
+        :class="
+        [
+            'selection:bg-primary',
+            'selection:text-primary-foreground',
+            'flex',
+            'h-9',
+            'w-full',
+            'min-w-0',
+            'rounded-md',
+            'border',
+            'px-3',
+            'py-1',
+            'text-base',
+            'transition-[color,box-shadow]',
+            'outline-none',
+            'disabled:pointer-events-none',
+            'disabled:cursor-not-allowed',
+            'disabled:opacity-50',
+            'md:text-sm',
+            'focus-visible:border-ring',
+            'focus-visible:ring-ring/50',
+            'focus-visible:ring-[3px]'
+            customClass
+        ]"
+    />
+</template>
+
+<style scoped>
+
+</style>
