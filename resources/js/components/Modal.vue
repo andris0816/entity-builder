55a8103334f5ec931fb368e083ca92e408bc005f
Bencsik András <acelbeton0101@gmail.com>
2025-10-17 07:44:51 +0200

    feat: modified the modal for more consistent styling

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import Button from "./Button.vue";
+import Card from "./Card.vue";
+
 interface Props {
     show: boolean;
 }
@@ -10,33 +13,42 @@ const props = withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <div
-        v-if="show"
-        class="fixed inset-0 bg-black/[.6] grid place-items-center"
+    <Transition
+        enter-from="opacity-0"
+        enter-to-class="opacity-100"
+        enter-active-class="transition duration-300"
+        leave-active-class="transition duration-200"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
     >
-        <div class="bg-gray-900 border-gray-800 p-4 rounded w-9/12 max-w-lg" @click.stop>
-            <header class="text-xl mb-4">
-                <slot name="header">
-                    <h2>Default Header</h2>
-                </slot>
-            </header>
+        <div
+            v-if="show"
+            class="fixed inset-0 bg-black/[.6] grid place-items-center"
+        >
+            <Card customClass="w-9/12 max-w-lg " @click.stop>
+                <header class="text-xl mb-4">
+                    <slot name="header">
+                        <h2>Default Header</h2>
+                    </slot>
+                </header>
 
-            <main>
-                <slot></slot>
-            </main>
+                <main>
+                    <slot></slot>
+                </main>
 
-            <footer class="pt-2 mt-2">
-                <slot name="footer">
-                    <button
-                        @click="$emit('close')"
-                        class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                    >
-                        Close
-                    </button>
-                </slot>
-            </footer>
+                <footer class="pt-2 mt-2">
+                    <slot name="footer">
+                        <Button
+                            @click="$emit('close')"
+                            variant="dark"
+                        >
+                            Cancel
+                        </Button>
+                    </slot>
+                </footer>
+            </Card>
         </div>
-    </div>
+    </Transition>
 </template>
 
 <style scoped>
