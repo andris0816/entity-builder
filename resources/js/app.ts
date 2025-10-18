0897da76e3dbc26adf1d62864e74a730a906fd6d
Bencsik András <acelbeton0101@gmail.com>
2025-10-18 14:20:14 +0200

    feat: switch the app file from javascript to typescript

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,9 @@
 import './bootstrap';
-import { createApp } from "vue";
+import {createApp, onMounted} from "vue";
 
 import App from "./App.vue";
-import {router} from "./router/index.js";
+import {router} from "./router";
 import {createPinia} from "pinia";
 
 const pinia = createPinia();
-
 createApp(App).use(router).use(pinia).mount("#app");
