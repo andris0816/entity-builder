aea356d54a694d9b39551c4ecdf82bab78beab6a
Bencsik András <acelbeton0101@gmail.com>
2025-10-18 14:23:10 +0200

    fix: renamed app to .ts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+{{--    <meta http-equiv="Content-Security-Policy" content="default-src 'self'">--}}
     <title>My Vue App</title>
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.ts', 'resources/css/app.css'])
 </head>
 <body>
 <div id="app" class="bg-gray-950 size-full dark"></div>
