0fdf63b555e1300738f16321bad66ea15fbec601
Bencsik András <acelbeton0101@gmail.com>
2025-10-22 20:19:37 +0200

    feat: created a relation for easier fetching

--- app/Models/World.php
+++ app/Models/World.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class World extends Model
 {
@@ -22,5 +23,10 @@ class World extends Model
     public function entities(): HasMany
     {
         return $this->hasMany(Entity::class, 'world_id');
+    }
+
+    public function relationships(): HasManyThrough
+    {
+        return $this->HasManyThrough(Relationship::class, Entity::class, 'world_id', 'entity_from', 'id', 'id');
     }
 }
