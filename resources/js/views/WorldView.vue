f69e10af3fa1cfbf2b5b22881d10539049e15dc2
Bencsik András <acelbeton0101@gmail.com>
2025-10-27 12:05:42 +0100

    feat: added error handling in api requests, and modified them a bit

--- resources/js/views/WorldView.vue
+++ resources/js/views/WorldView.vue
@@ -7,10 +7,12 @@ import Map from "../components/WorldMap/Map.vue";
 import RelationshipCreate from "../components/WorldMap/RelationshipCreate.vue";
 import {Entity} from "../types/entity";
 import {Relationship} from "../types/relationship";
+import {ValidationErrors} from "../types/ValidationErrors";
 
 const route = useRoute();
 const entities = ref<Entity[]>([]);
 const relationships = ref<Relationship[]>([]);
+const errors = ref<ValidationErrors>({});
 
 onMounted(async() => {
    const response = await apiFetch(`/worlds/${route.params.id}`);
@@ -37,13 +39,15 @@ const saveEntity = async (formData: any) => {
 
         const data = await response.json();
 
-        console.log(data);
+        entities.value.push(data);
     } catch (err) {
         console.error(err);
     }
 }
 
 const saveRelationship = async (formData: any) => {
+    errors.value = {};
+
     try {
         formData.world_id = route.params.id;
         const response = await apiFetch('/relationship', {
@@ -51,13 +55,22 @@ const saveRelationship = async (formData: any) => {
             body: JSON.stringify(formData)
         });
 
-        if (!response.ok) throw new Error('Failed to save relationsip');
-
         const data = await response.json();
 
-        console.log(data);
+        if (!response.ok) {
+            if (response.status === 422) {
+                errors.value = data.errors || {};
+            } else {
+                console.error('Server error:', data.message || data);
+            }
+            return;
+        }
+
+        relationships.value.push(data);
+
+        errors.value = {};
     } catch (err) {
-        console.error(err);
+        console.error(err)
     }
 }
 
@@ -71,10 +84,17 @@ const simplifiedEntities = computed(() => {
     <div class="flex-1 flex overflow-hidden">
         <div class="w-[280px] bg-gray-900 border-r border-gray-800 h-full overflow-y-auto p-6 space-y-8">
             <div class="space-y-4">
-                <EntityCreate @submit="saveEntity"></EntityCreate>
+                <EntityCreate
+                    @submit="saveEntity"
+                    :errors="errors"
+                />
             </div>
             <div class="space-y-4">
-                <RelationshipCreate @submit="saveRelationship" :entities="simplifiedEntities"></RelationshipCreate>
+                <RelationshipCreate
+                    @submit="saveRelationship"
+                    :entities="simplifiedEntities"
+                    :errors="errors"
+                />
             </div>
         </div>
         <div class="flex-1 bg-gray-950 h-full relative">
