36969b646348b274837b28481c8f80a66f309b3a
Bencsik András <acelbeton0101@gmail.com>
2025-10-30 11:08:12 +0100

    feat: created an entity type component and a data file for it

---
+++ resources/js/components/EntityType.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {entityColorClasses} from "../data/entityColor";
+import {computed} from "vue";
+
+const props = defineProps<{
+    type: string;
+}>();
+
+const colorClass = computed(() => entityColorClasses[props.type] || "gray-500");
+</script>
+
+<template>
+    <span
+        :class="[
+                `inline-flex items-center justify-center rounder-md
+                    border px-2 py-0.5 text-xs font-medium w-fit
+                    whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none`,
+                `bg-${colorClass}/20`,
+                `text-${colorClass}/50`,
+                `border-${colorClass}`,
+        ]"
+    >
+        {{ type }}
+    </span>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/data/entityColor.ts
@@ -0,0 +1,6 @@
+export const entityColorClasses: Record<string, string> = {
+    Character: "blue-500",
+    Location: "green-500",
+    Item: "yellow-500",
+    Event: "orange-500",
+};
