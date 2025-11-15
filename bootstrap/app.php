a8ea618d9dad56a9fd656d793550cbd5f83c233d
Bencsik András <acelbeton0101@gmail.com>
2025-11-15 19:46:22 +0100

    feat: remove fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,4 +21,4 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create()->make('cache')->clear();
+    })->create();
