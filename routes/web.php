8b778ce62dfb57e1a362c030744d61edfde5e485
Bencsik András <acelbeton0101@gmail.com>
2025-11-15 19:51:41 +0100

    feat: temporary endpoint to check deployment issue

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/debug-app-url', function () {
+    return config('app.url');
+});
 
 Route::get('/{any}', function () {
     return view('app');
